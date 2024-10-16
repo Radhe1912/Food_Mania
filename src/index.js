37ad78f694ded154c06a7ad14ba658659a92fda8
Radhe1912 <106031414+Radhe1912@users.noreply.github.com>
2024-10-16 22:31:33 +0530

    Your comment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './frontend/App';
+import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
-import reportWebVitals from './frontend/reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
